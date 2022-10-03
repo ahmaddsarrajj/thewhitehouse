624bd2fc1690e214c0f2841e4f277bdc0f421183
ahmad sarraj <83416111+ahmaddsarrajj@users.noreply.github.com>
2022-10-03 11:31:37 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
+        <footer id="contact-us">
             <div className='contact-us' >
       
                 <div class="blockquote-wrapper">
